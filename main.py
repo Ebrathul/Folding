34483b9ca8a7323ac038ba15e49ae2f35b6b512c
ebrit <Ebrithil384@hotmail.de>
2020-05-28 18:07:15 +0200

    Forces for Bonds and Angles defined. Initialization has own method

--- main.py
+++ main.py
@@ -7,15 +7,58 @@ class Protein():
         self.sequence = self.get_sequence()
 
 
-    def force(self,  type=None):
-        k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c  = self.params
+    def force(self, pos, n, atom, type=None):  # pos must be array [3, 3, n]
+
+        k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c = self.params
         if type == "bond":
-            return 2 * k_b * (b - b_zero)
+            if atom != 0:
+                b_x = pos[0, atom, n] - pos[0, atom - 1, n]
+                b_y = pos[1, atom, n] - pos[1, atom - 1, n]
+            elif n != 0:  # previous amino
+                b_x = pos[0, atom, n] - pos[0, 2, n-1]
+                b_y = pos[1, atom, n] - pos[1, 2, n-1]
+            else:  # redundant, n=0, atom=0 needs to be forbidden
+                print("Oups this schould not be used!")
+                b_x = pos[0, atom, n] - pos[0, 0, 0]  # should return zero
+                b_y = pos[1, atom, n] - pos[1, 0, 0]  # should return zero
+            # calculation of force angle
+            angle_b_x_y = np.arctan(b_x / b_y)
+            b_zero_x = b_zero * np.cos(angle_b_x_y)
+            b_zero_y = b_zero * np.sin(angle_b_x_y)
+            f_bound_x, f_bound_y = 2 * k_b * (b_x - b_zero_x), 2 * k_b * (b_y - b_zero_y)
+            return f_bound_x, f_bound_y
+
 
         if type == "angle":
-            return 2 * k_phi * (a - a_zero)
+           if n != 0:
+                if atom == 0:
 
-        if type == "torsion":
+                    d = np.sqrt((pos[0, atom, n] - pos[0, 2, n - 1])^2 + (pos[1, atom, n] - pos[1, 2, n - 1])^2)       # distance between first and second atom
+
+                    e = np.sqrt((pos[0, atom+1, n] - pos[0, atom, n])^2 + (pos[1, atom +1, n] - pos[1, atom, n ])^2)   # distance between second and third atom
+
+                    f = np.sqrt((pos[0, atom+1, n] - pos[0, 2, n - 1])^2 + (pos[1, atom + 1, n] - pos[1, 2, n - 1])^2) # distance between first and third atom
+
+
+                elif atom == 1:
+
+                    d = np.sqrt((pos[0, atom, n] - pos[0, atom - 1, n - 1]) ^ 2 + (pos[1, atom, n] - pos[1, atom - 1, n]) ^ 2)  # distance between first and second atom
+
+                    e = np.sqrt((pos[0, atom + 1, n] - pos[0, atom, n]) ^ 2 + (pos[1, atom + 1, n] - pos[1, atom, n]) ^ 2)  # distance between second and third atom
+
+                    f = np.sqrt((pos[0, atom + 1, n] - pos[0, atom - 1, n]) ^ 2 + (pos[1, atom + 1, n] - pos[1, atom - 1, n]) ^ 2)  # distance between first and third atom
+
+                elif atom == 2:
+                    d = np.sqrt((pos[0, atom, n] - pos[0, atom - 1, n - 1]) ^ 2 + (pos[1, atom, n] - pos[1, atom - 1, n]) ^ 2)  # distance between first and second atom
+
+                    e = np.sqrt((pos[0, 0, n + 1] - pos[0, atom, n]) ^ 2 + (pos[1, 0, n + 1] - pos[1, atom, n]) ^ 2)  # distance between second and third atom
+
+                    f = np.sqrt((pos[0, 0, n + 1] - pos[0, atom - 1, n]) ^ 2 + (pos[1, 0, n + 1] - pos[1, atom - 1, n]) ^ 2)  # distance between first and third atom
+
+        a = np.arccos(d ^ 2 + e ^ 2 - f ^ 2 / (2 * d * e))
+        return 2 * k_phi * (a - a_zero), 2 * k_phi * (a - a_zero)  # torque
+
+        if type == "torsion":  # not included yet, 2d impossible
             return -k_phi * n_phi * np.sin(n_phi * phi + delta)
 
         if type == "elec_lenard":
@@ -40,35 +83,13 @@ class Protein():
 
 
     def get_position(self, n):
-        if n-1 < 0:
-            pass
-        else:
-            pass
+        return pos(n)
 
 
-    def set_position(self, n):
+
+    def update_position(self, n, t):
         k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c = self.params
-        pos = self.amino(n)
-        if n > 0:
-            pos_bef = self.amino(n-1)
-        if n%2 == 0:
-            zickzack = -1
-        else:
-            zickzack = 1
-        for i in range(len(pos[:, 0])):
-            b = np.random.randint(130, 160)
-            a = np.radians(np.random.randint(90, 180))
-            if i == 0:
-                if n > 0:
-                    pos[0, i] = pos_bef[0, 2] + (b * np.sin(-a * zickzack))
-                    pos[1, i] = pos_bef[1, 2] + (b * np.cos(-a * zickzack))
-            if i == 1:
-                pos[0, i] = pos[0, i-1] + (b * np.sin(a * zickzack))
-                pos[1, i] = pos[1, i-1] + (b * np.cos(a * zickzack))
-            if i == 2:
-                pos[0, i] = pos[0, i-1] + (b * np.sin(-a * zickzack))
-                pos[1, i] = pos[1, i-1] + (b * np.cos(-a * zickzack))
-        return pos
+
 
         # else:
         #     pos_bond = self.amino(n - 1)
@@ -78,37 +99,35 @@ class Protein():
         #     pass
 
 
-    def initialize_positions(self, n):
-        k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c = self.params
-        pos = self.amino(n)
-        if n > 0:
-            pos_bef = self.amino(n-1)
-        if n % 2 == 0:
-            zickzack = -1
-        else:
-            zickzack = 1
-        for i in range(len(pos[:, 0])):
-            b = np.random.randint(130, 160)
-            a = np.radians(np.random.randint(90, 180))
-            if i == 0:
-                if n > 0:
-                    pos[0, i] = pos_bef[0, 2] + (b * np.cos(-a * zickzack))
-                    pos[1, i] = pos_bef[1, 2] + (b * np.sin(-a * zickzack))
-            if i == 1:
-                pos[0, i] = pos[0, i-1] + (b * np.cos(a * zickzack))
-                pos[1, i] = pos[1, i-1] + (b * np.sin(a * zickzack))
-            if i == 2:
-                pos[0, i] = pos[0, i-1] + (b * np.cos(-a * zickzack))
-                pos[1, i] = pos[1, i-1] + (b * np.sin(-a * zickzack))
-        return pos
-
-
-    def structure(self):
+    def initialize_positions(self):
         structure = []
         for n in range(len(self.sequence)):
-            print(self.initialize_positions(n))
-            structure.append(self.initialize_positions(n))
+            k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c = self.params
+            pos = self.amino(n)
+            if n > 0:
+                pos_bef = self.amino(n - 1)
+            if n % 2 == 0:
+                zickzack = -1  # changes direction of rotation
+            else:
+                zickzack = 1
+            for i in range(len(pos[:, 0])):
+                b = np.random.randint(130, 160)  # create random distance between atoms
+                a = np.radians(np.random.randint(90, 180))  # create random angle between atom (in radiant)
+                if i == 0:
+                    if n > 0:
+                        pos[0, i] = pos_bef[0, 2] + (b * np.cos(-a * zickzack))
+                        pos[1, i] = pos_bef[1, 2] + (b * np.sin(-a * zickzack))
+                if i == 1:
+                    pos[0, i] = pos[0, i-1] + (b * np.cos(a * zickzack))
+                    pos[1, i] = pos[1, i-1] + (b * np.sin(a * zickzack))
+                if i == 2:
+                    pos[0, i] = pos[0, i-1] + (b * np.cos(-a * zickzack))
+                    pos[1, i] = pos[1, i-1] + (b * np.sin(-a * zickzack))
+            np.array(structure.append(pos))
         return structure
+
+
+
 
 
 k_b = 1
@@ -122,6 +141,9 @@ k_phi, n_phi, phi, delta, r, qi, qj, a, c = 1, 1, 1, 1, 1, 1, 1, 1, 1
 
 params = [k_b, b, b_zero, k_a, a, a_zero, k_phi, n_phi, phi, delta, r, qi, qj, a, c]
 
-carbonchain = Protein(params, presequence="XXXXX")
+carbonchain = Protein(params, presequence="XXXXXXX")
 
-print(carbonchain.structure())
+pos = carbonchain.initialize_positions()
+print(pos)
+
+#np.dot(b_x,b_y)/ (np.linalg.norm(b_x) * np.linalg.norm(b_y))
